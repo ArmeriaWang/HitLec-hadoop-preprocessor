cb5c41e323246b5a799629c39185c7919da33406
Wang Hankun <armeriaw@outlook.com>
2021-04-16 22:30:23 +0800

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,12 +1,16 @@
 project_path="/home/armeria/Work/courses/db_calc/Lab1-1183710106"
 java_source_path=$project_path"/src/main/java"
 
-round=0
+run_all=0
+run_single=0
 init=0
-while getopts "r:i" arg; do
+while getopts "a:s:i" arg; do
     case $arg in
-    r)
-        round=$OPTARG
+    a)
+        run_all=$OPTARG
+        ;;
+    s)
+        run_single=$OPTARG
         ;;
     i)
         init=1
@@ -17,6 +21,18 @@ while getopts "r:i" arg; do
         ;;
     esac
 done
+
+printf "Plan to run: "
+if [ "$run_all" -ge 1 ] || [ "$run_single" -eq 1 ]; then
+    printf "Sampler"
+fi
+if [ "$run_all" -ge 2 ] || [ "$run_single" -eq 2 ]; then
+    printf ", Filter"
+fi
+if [ "$run_all" -ge 1 ] || [ "$run_single" -eq 3 ]; then
+    printf ", MinMax"
+fi
+printf "\n"
 
 cd $java_source_path || exit
 if [ $init -eq 1 ]; then
@@ -35,8 +51,8 @@ hadoop com.sun.tools.javac.Main -d ./ ./*.java
 # Pack as main.jar
 jar cf main.jar *.class
 
-if [ "$round" -ge 1 ]; then
-    # Prepare and run 1st round: sampler
+if [ "$run_all" -ge 1 ] || [ "$run_single" -eq 1 ]; then
+    # Prepare and run 1st run_all]: sampler
     hdfs dfs -rm -r /user/armeria/bdclab1/sampler_output
     hadoop jar main.jar Sampler /user/armeria/bdclab1/input /user/armeria/bdclab1/sampler_output
     # hdfs dfs -cat /user/armeria/bdclab1/output/part-r-00000 | head -n 20
@@ -49,8 +65,8 @@ if [ "$round" -ge 1 ]; then
 # zip -q results_0.zip part-r-00000 debug_info_0.txt real_samples_0.txt
 fi
 
-if [ "$round" -ge 2 ]; then
-    # Prepare and run 2st round: filter
+if [ "$run_all" -ge 2 ] || [ "$run_single" -eq 2 ]; then
+    # Prepare and run 2st run_all]: filter
     cd $java_source_path || exit
     hdfs dfs -rm -r /user/armeria/bdclab1/filter_output
     hadoop jar main.jar Filter /user/armeria/bdclab1/sampler_output /user/armeria/bdclab1/filter_output
