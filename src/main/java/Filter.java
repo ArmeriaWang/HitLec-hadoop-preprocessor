5e69e652b23f8292e435f36e9c8cf26bbe3ab661
Wang Hankun <armeriaw@outlook.com>
2021-04-16 22:37:44 +0800

    Update Filter.java

--- src/main/java/Filter.java
+++ src/main/java/Filter.java
@@ -1,5 +1,6 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -12,21 +13,21 @@ import java.util.StringTokenizer;
 
 public class Filter {
 
-    public static class FilterMapper extends Mapper<Object, Text, CareerWritable, ReviewWritable> {
+    public static class FilterMapper extends Mapper<Object, Text, NullWritable, ReviewWritable> {
         @Override
         protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             StringTokenizer itr = new StringTokenizer(value.toString(), "\n");
             while (itr.hasMoreTokens()) {
                 String rawString = itr.nextToken();
                 ReviewWritable review = new ReviewWritable(rawString);
-                context.write(CareerWritable.valueOf(review.getUserCareer()), review);
+                context.write(NullWritable.get(), review);
             }
         }
     }
 
-    public static class FilterReducer extends Reducer<CareerWritable, ReviewWritable, CareerWritable, ReviewWritable> {
+    public static class FilterReducer extends Reducer<NullWritable, ReviewWritable, NullWritable, ReviewWritable> {
         @Override
-        protected void reduce(CareerWritable key, Iterable<ReviewWritable> reviews, Context context)
+        protected void reduce(NullWritable key, Iterable<ReviewWritable> reviews, Context context)
                 throws IOException, InterruptedException {
             for (ReviewWritable review : reviews) {
                 if (isLegalLatitude(review.getLatitude()) && isLegalLongitude((review.getLongitude()))) {
@@ -51,9 +52,9 @@ public class Filter {
         job.setMapperClass(FilterMapper.class);
 //        job.setCombinerClass(SampleReducer.class);
         job.setReducerClass(FilterReducer.class);
-        job.setMapOutputKeyClass(CareerWritable.class);
+        job.setMapOutputKeyClass(NullWritable.class);
         job.setMapOutputValueClass(ReviewWritable.class);
-        job.setOutputKeyClass(CareerWritable.class);
+        job.setOutputKeyClass(NullWritable.class);
         job.setOutputValueClass(ReviewWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
