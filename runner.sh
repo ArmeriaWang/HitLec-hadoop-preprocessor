bf07ffc71d897ec8193324d8c8ff72cb4b82eb12
Wang Hankun <armeriaw@outlook.com>
2021-05-03 17:39:46 +0800

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -104,7 +104,7 @@ if [ "$run_all" -ge 1 ] || [ "$run_single" -eq 1 ]; then
     # Prepare and run 1st: sampler
     echo "Sample start"
     hdfs dfs -rm -r $bdclab1_hpath_regular/sampler_output
-    hadoop jar main.jar Sampler $bdclab1_hpath/input $bdclab1_hpath_regular/sampler_output 0.005
+    hadoop jar main.jar regular.Sampler $bdclab1_hpath/input $bdclab1_hpath_regular/sampler_output 0.005
     # hdfs dfs -cat $bdclab1_hpath_regular/output/part-r-00000 | head -n 20
     cd $project_path || exit
     rm -rf ./sampler_output
