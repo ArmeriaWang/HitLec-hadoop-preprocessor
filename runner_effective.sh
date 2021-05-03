7776cd030f17491f11c023c99e6b9aa9ed2e0d18
Wang Hankun <armeriaw@outlook.com>
2021-05-03 20:22:32 +0800

    Update runner_effective.sh

--- runner_effective.sh
+++ runner_effective.sh
@@ -101,7 +101,7 @@ if [ "$run_all" -ge 1 ] || [ "$run_single" -eq 1 ]; then
     hadoop jar main.jar effective.SampleFilterMinMax $bdclab1_hpath/input  `# class name, input path` \
         $bdclab1_hpath_effective/sample_filter_minmax_output 0.005         `# output path, sample rate` \
         $bdclab1_hpath_effective/sample_filter_minmax_output/minmax.txt    `# path of minmax.txt`
-    cd mkdir -p $local_results_path || exit
+    cd $local_results_path || exit
     rm -rf ./sample_filter_minmax_output
     hadoop fs -copyToLocal $bdclab1_hpath_effective/sample_filter_minmax_output .
     if [ ! -d "sample_filter_minmax_output" ]; then
